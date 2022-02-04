961998db3e25d014a2cbea0a5a752a5e0473ac9c
Markys97 <70927348+Markys97@users.noreply.github.com>
2022-02-04 21:38:09 +0300

    Add files via upload

--- myJs/slyder.js
+++ myJs/slyder.js
@@ -1,17 +1,28 @@
 let burgerButton= document.querySelector('.header-bottom__burger');
-let menu= document.querySelector('.header-bottom-navbar');
+let nav= document.querySelector('.header-bottom-navbar');
 let closeButton= document.querySelector('.header-bottom__close-btn');
+let menu= document.querySelector('.header-bottom-menu');
+
 
 
 burgerButton.addEventListener('click',e=>{
+  nav.classList.add('show-menu')
   menu.classList.add('show-menu')
+
+
+
   closeButton.classList.add('visible')
   
 
 })
 
 closeButton.addEventListener('click',e=>{
+  nav.classList.remove('show-menu')
   menu.classList.remove('show-menu')
+
+
+
+ 
   closeButton.classList.remove('visible')
   
 
